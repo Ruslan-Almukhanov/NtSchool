ad5f4f2bcf36e2b62b5d671cb939e0d8c968664e
Ruslan Almukhanov <ruslanalmukhanov@gmail.com>
2018-12-08 15:43:28 +0500

    добавил две модели Post и Category

--- migration.php
+++ migration.php
@@ -11,25 +11,56 @@ require_once 'config/database.php';
 //    $table->timestamps();
 //});
 
-Illuminate\Database\Capsule\Manager::schema()->create('posts', function ($table) {
-    $table->increments('id');
-    $table->string('title');
-    $table->string('description');
-    $table->text('content');
-    $table->string('cover');
-    $table->timestamps();
-});
+//Illuminate\Database\Capsule\Manager::schema()->create('posts', function ($table) {
+//    $table->increments('id');
+//    $table->string('title');
+//    $table->string('description');
+//    $table->text('content');
+//    $table->string('cover');
+//    $table->timestamps();
+//});
+//
+//Illuminate\Database\Capsule\Manager::schema()->create('categories', function ($table) {
+//    $table->increments('id');
+//    $table->string('title');
+//    $table->string('key');
+//    $table->timestamps();
+//});
+//
+//Illuminate\Database\Capsule\Manager::schema()->create('category_post', function ($table) {
+//    $table->integer('post_id');
+//    $table->integer('category_id');
+//    $table->timestamps();
+//    $table->primary(['post_id','category_id']);
+//});
 
-Illuminate\Database\Capsule\Manager::schema()->create('categories', function ($table) {
-    $table->increments('id');
-    $table->string('title');
-    $table->string('key');
-    $table->timestamps();
-});
+$category = new \NtSchool\Model\Category();
+$category->title = 'Category 1';
+$category->key = 'category-1';
+$category->save();
 
-Illuminate\Database\Capsule\Manager::schema()->create('category_post', function ($table) {
-    $table->integer('post_id');
-    $table->integer('category_id');
-    $table->timestamps();
-    $table->primary(['post_id','category_id']);
-});
+$category = new \NtSchool\Model\Category();
+$category->title = 'Category 2';
+$category->key = 'category-2';
+$category->save();
+
+$category = new \NtSchool\Model\Category();
+$category->title = 'Category 3';
+$category->key = 'category-3';
+$category->save();
+
+$data = [
+    'title' => 'Post',
+    'description' => 'Post Description',
+    'content' => 'Post Content',
+    'cover' => 'http://'
+];
+
+for ($counter = 1; $counter <= 15; $counter ++) {
+    $post = new \NtSchool\Model\Post();
+    $post->title = $data['title'] . ' ' . $counter;
+    $post->description = $data['description'] . ' ' . $counter;
+    $post->content = $data['content'] . ' ' . $counter;
+    $post->cover = $data['cover'] . ' ' . $counter;
+    $post->save();
+}
---
+++ src/Model/Post.php
@@ -0,0 +1,9 @@
+<?php
+namespace NtSchool\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+
+}

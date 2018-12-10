797df602b9eec71878f1104743582cde90388959
Ruslan Almukhanov <ruslanalmukhanov@gmail.com>
2018-12-10 21:54:06 +0500

    добавил папку cache  в репозиторий

--- config/database.php
+++ config/database.php
@@ -6,10 +6,10 @@ $capsule = new Capsule;
 
 $capsule->addConnection([
     'driver'    => 'mysql',
-    'host'      => 'rus',
-    'database'  => 'ntsholl',
-    'username'  => 'root',
-    'password'  => '',
+    'host'      => env('DATABASE_HOST'),
+    'database'  => env('DATABASE_DBNAME'),
+    'username'  => env('DATABASE_USERNAME'),
+    'password'  => env('DATABASE_PASSWORD'),
     'charset'   => 'utf8',
     'collation' => 'utf8_unicode_ci',
     'prefix'    => '',
